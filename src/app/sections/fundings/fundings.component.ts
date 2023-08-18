0e8af33a5ec562d4c34b10aa40ee915ba7cdc87c
Samba Mutthoju <samba@amnetdigital.com>
2023-08-18 17:12:59 +0530

    Funding ts is code formatted

--- src/app/sections/fundings/fundings.component.ts
+++ src/app/sections/fundings/fundings.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ModalService } from '../_modal';
-import { CustomerService } from 'src/app/services/customerService';
-import { DatePipe } from '@angular/common';
-import { Firmographics } from 'src/app/classesList/customer';
-import { DashboardService } from 'src/app/services/dashboardService';
+import { Component, OnInit, Input } from "@angular/core";
+import { ModalService } from "../_modal";
+import { CustomerService } from "src/app/services/customerService";
+import { DatePipe } from "@angular/common";
+import { Firmographics } from "src/app/classesList/customer";
+import { DashboardService } from "src/app/services/dashboardService";
 
 @Component({
-  selector: 'app-fundings',
-  templateUrl: './fundings.component.html',
-  styleUrls: ['./fundings.component.css']
+  selector: "app-fundings",
+  templateUrl: "./fundings.component.html",
+  styleUrls: ["./fundings.component.css"],
 })
 export class FundingsComponent implements OnInit {
   @Input() isFull: boolean;
@@ -16,68 +16,74 @@ export class FundingsComponent implements OnInit {
   modalName: string;
   cardTitle: string;
   allHeadings: any;
-  funding_tooltip: string = 'Fundings related to your book of business';
+  funding_tooltip: string = "Fundings related to your book of business";
 
-  customersFundedData: Firmographics[] = []
-  constructor(private dashboardService: DashboardService,private modalService: ModalService, private service: CustomerService, private datePipe: DatePipe) { 
+  customersFundedData: Firmographics[] = [];
+
+  constructor(
+    private dashboardService: DashboardService,
+    private modalService: ModalService,
+    private service: CustomerService,
+    private datePipe: DatePipe
+  ) {
     this.getAllHedings();
     this.getfirmographicsData();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-   // method for getAllHeadings
-   getAllHedings() {
-      if(this.dashboardService.storeAllheadings === undefined) {
-      this.dashboardService.getAllheadings().subscribe(headings => {
+  // method for getAllHeadings
+  getAllHedings() {
+    if (this.dashboardService.storeAllheadings === undefined) {
+      this.dashboardService.getAllheadings().subscribe((headings) => {
         this.allHeadings = headings;
         this.modalName = this.allHeadings.fundingId;
-       })
-     } else {
+      });
+    } else {
       this.allHeadings = this.dashboardService.storeAllheadings;
-       this.modalName = this.allHeadings.fundingId;
-     }
-    
+      this.modalName = this.allHeadings.fundingId;
+    }
   }
 
   // method used for open the widget into modal popup to show entire data.
   openModal() {
     this.modalService.open(this.modalName);
-   }
+  }
 
-   // method used for close the widget modal popup.
-   closeModal() {
+  // method used for close the widget modal popup.
+  closeModal() {
     this.modalService.close(this.modalName);
   }
 
-  sortFundingsOnDate(){
+  sortFundingsOnDate() {
     return this.customersFundedData.sort((a, b) => {
-        return <any>new Date(b.Funding_Announced_On) - <any>new Date(a.Funding_Announced_On);
+      return (
+        <any>new Date(b.Funding_Announced_On) -
+        <any>new Date(a.Funding_Announced_On)
+      );
     });
-   }
+  }
 
   // method for all calling the funds data for all customer
-
   getfirmographicsData() {
     let fundingData = [];
-    this.service.getAllfirmographics().subscribe(firmo => {
-       firmo.forEach(firmoGraphicData => {
-          let changeMoney = firmoGraphicData.Total_Funding_Amount;
-        });
+    this.service.getAllfirmographics().subscribe((firmo) => {
+      firmo.forEach((firmoGraphicData) => {
+        let changeMoney = firmoGraphicData.Total_Funding_Amount;
+      });
 
       firmo.sort((a, b) => {
-          if (a.customer_name < b.customer_name) return -1;
-          else if (a.customer_name > b.customer_name) return 1;
-          else return 0;
-        });
-        fundingData = firmo;
+        if (a.customer_name < b.customer_name) return -1;
+        else if (a.customer_name > b.customer_name) return 1;
+        else return 0;
+      });
+      fundingData = firmo;
 
-        fundingData.forEach(fund => {
-           if(fund.Money_Raised != '') {
-             this.customersFundedData.push(fund);
-           }
-        });
-      })
+      fundingData.forEach((fund) => {
+        if (fund.Money_Raised != "") {
+          this.customersFundedData.push(fund);
+        }
+      });
+    });
   }
 }
