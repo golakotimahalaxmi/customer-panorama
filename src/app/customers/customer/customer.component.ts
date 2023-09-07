0e7170b437a6dd45368b982c6f97e561180b14e8
Samba Mutthoju <samba@amnetdigital.com>
2023-09-07 14:54:23 +0530

    customer-identity Ui changes are updated

--- src/app/customers/customer/customer.component.ts
+++ src/app/customers/customer/customer.component.ts
@@ -14,27 +14,95 @@ export class CustomerComponent implements OnInit {
 
   refreshCustomerData: boolean;
   widgetPositions: widget[];
-
+  stackHolderName: string;
+  insightFlage: boolean = false;
   constructor(private route: ActivatedRoute, private service: CustomerService, private dashboardService: DashboardService) {
-    this.dashboardService.pageName = "customer"
-    // this.widgetPositions;
-    // this.stackHolderName = "healthcare";   
+    this.dashboardService.pageName = "customer"   
   }
 
   ngOnInit() {
-    if (this.service.customerType === "smallbusiness") {
-      this.dashboardService.widgetPositions =
-        this.dashboardService.corporate_Widget;
-      this.widgetPositions = this.dashboardService.widgetPositions;
-    } else if (this.service.customerType === "transportation") {
-      this.dashboardService.widgetPositions =
-        this.dashboardService.small_biz_Widget;
-      this.widgetPositions = this.dashboardService.widgetPositions;
-    } else {
-      this.dashboardService.widgetPositions =
-        this.dashboardService.small_business_Widget;
-      this.widgetPositions = this.dashboardService.widgetPositions;
-    }   
+    let item = [];
+    this.dashboardService.widgetPositions =
+      this.dashboardService.corporate_Widget;
+    this.widgetPositions = this.dashboardService.widgetPositions;
+    const paramURL = window.location.href.split("/");
+    item = JSON.parse(sessionStorage.getItem("insights"))     
+    item.filter(k => {     
+      if (k.customer_code === +paramURL[paramURL.length-1])       
+        this.insightFlage = true;
+       });
+    // switch (this.service.customerType) {
+    //   case "corporate": {
+    //     this.stackHolderName = "corporate";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "healthcare": {
+    //     this.stackHolderName = "healthcare";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "smallbusiness": {
+    //     this.stackHolderName = "smallbusiness";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.small_business_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "food-beverages": {
+    //     this.stackHolderName = "food-beverages";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "retail": {
+    //     this.stackHolderName = "retail";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "insurance": {
+    //     this.stackHolderName = "insurance";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "technology": {
+    //     this.stackHolderName = "technology";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "petroleum": {
+    //     this.stackHolderName = "petroleum";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "transportation": {
+    //     this.stackHolderName = "transportation";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    //   case "automotive": {
+    //     this.stackHolderName = "automotive";
+    //     this.dashboardService.widgetPositions =
+    //       this.dashboardService.corporate_Widget;
+    //     this.widgetPositions = this.dashboardService.widgetPositions;
+    //     break;
+    //   }
+    // }     
   }
 
   onDrop(event: CdkDragDrop<string[]>) {
